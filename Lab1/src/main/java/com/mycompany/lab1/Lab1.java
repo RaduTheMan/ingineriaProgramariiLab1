d4698f9cffa27fa7ffe9cf48ea3b68326904462c
RaduTheMan <radu.damian07@gmail.com>
2021-02-20 17:32:17 +0200

    rafinare Lab1.java

--- Lab1/src/main/java/com/mycompany/lab1/Lab1.java
+++ Lab1/src/main/java/com/mycompany/lab1/Lab1.java
@@ -15,10 +15,17 @@ public class Lab1 {
     {
         Cal myCal = new Iapa("munte", "Rocinante", 13, 480, 1.3, 54);
         Cal myCal2 = new Cal();
+        Cal myCal3 = new Cal("ferma", "Pegasus", 30, 530, 1.8, 90);
+        
         myCal.printAction();
         myCal2.printAction();
         myCal.printInfo();
         myCal2.printInfo();
+        
+        myCal2.setName("Babusca");
+        myCal2.printInfo();
+        
+        myCal3.printInfo();
         
     }
     
